d9c3087861bf164e0726796a83c66e2586867670
Your Name <you@example.com>
2022-11-05 17:12:31 -0300

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
+
 
 function App() {
+  const [displayTime, setDisplayTime] = useState(25 * 60)
+  const formatTime = (time) => {
+    let minutes = Math.floor(time / 60)
+    let seconds = time % 60
+    return (
+      (minutes < 10 ? "0" + minutes : minutes) +
+      ":" +
+      (seconds < 10 ? "0" + seconds : seconds)
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>25 + 5 Clock</h1>
+      
+      <h2 id="break-label">Breack Session Length</h2>
+      <button id="break-decrement">-</button>
+      <span id="break-length">5</span>
+      <button id="break-increment">+</button>
+
+      <h2 id="session-label">Session Length</h2>
+      <button id="session-decrement">-</button>
+      <span id="time-left">25</span>
+      <button id="session-increment">+</button>
+
+      <h2 id="timer-label">Session</h2>
+      <h2 id="session-length">{formatTime(displayTime)}</h2>
+
+      <button id="start_stop">play / pause</button>
+      <button id="reset">Reset</button>
     </div>
   );
 }
